28df20d01f8a333030096dfe0cf924fb0aa4b8a9
melchanatan <chanatan@outlook.com>
2024-03-12 20:58:50 +0700

    feat: added publish page

---
+++ app/publish/page.tsx
@@ -0,0 +1,166 @@
+"use client";
+import Dropzone from "@/components/Dropzone";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+const PublishPage = () => {
+  const router = useRouter();
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    name: "",
+    description: "",
+    tag: "",
+    image: "",
+    stockMax: 0,
+    stockCurrent: 0,
+  });
+
+  const createItem = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+
+    // try {
+    //   const response = await fetch("/api/item/new", {
+    //     method: "POST",
+    //     body: JSON.stringify({
+    //       name: post.name,
+    //       // userId: session?.user.id,
+    //       description: post.description,
+    //       tag: post.tag,
+    //       // image: post.imageUrl,
+    //       stockMax: post.stockMax,
+    //       stockCurrent: post.stockMax,
+    //     }),
+    //   });
+    //   if (response.ok) {
+    //     router.push("/admin");
+    //   }
+    // } catch (error) {
+    //   console.log(error);
+    // } finally {
+    //   setSubmitting(false);
+    // }
+  };
+
+  return (
+    <section className="main-container">
+      <h1 className="head_text text-left">
+        <span> Item</span>
+      </h1>
+      <p className="desc text-left max-w-md">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum, ex id!
+        Fuga quos illum vel iusto odio nulla molestias hic?
+      </p>
+
+      <form className="mt-10 w-full max-w-screen-2xl flex flex-col gap-7 box-container">
+        <label>
+          <span className="font-semibold text-gray-700">หัวข้องานวิจัย</span>
+          <input
+            value={post.name}
+            onChange={(e) => setPost({ ...post, name: e.target.value })}
+            placeholder="Enter your Item name"
+            required
+            className="form-input"
+          />
+        </label>
+
+        {/* <label>
+          <span className=" font-semibold text-base text-gray-700"></span>
+          <input
+            value={post.stockMax}
+            onChange={(e) => setPost({ ...post, stockMax: e.target.value })}
+            placeholder="พิมพ์เลย"
+            required
+            type="number"
+            className="w-2 form-input"
+          />
+        </label> */}
+        <label>
+          <span className=" font-semibold text-base text-gray-700">
+            บทคัดย่องานวิจัย
+          </span>
+          <textarea
+            value={post.description}
+            onChange={(e) => setPost({ ...post, description: e.target.value })}
+            placeholder="พิมพ์ตรงนี้..."
+            className="form-textarea"
+          />
+        </label>
+
+        <label>
+          <span className=" font-semibold text-base text-gray-700">
+            หมวดหมู่งานวิจัย{" "}
+            <span className="font-normal">(#sensor, #acuator, #mechanic)</span>
+          </span>
+          <input
+            value={post.tag}
+            onChange={(e) => setPost({ ...post, tag: e.target.value })}
+            placeholder="#tag"
+            required
+            className="form-input"
+          />
+        </label>
+
+        <label>
+          <span className=" font-semibold text-base text-gray-700">องกรณ์</span>
+          <input
+            value={post.name}
+            onChange={(e) => setPost({ ...post, name: e.target.value })}
+            placeholder="Enter your Item name"
+            required
+            className="form-input"
+          />
+        </label>
+
+        <label>
+          <span className=" font-semibold text-base text-gray-700">
+            ชื่อผู้ค้นคว้า
+          </span>
+          <input
+            value={post.name}
+            onChange={(e) => setPost({ ...post, name: e.target.value })}
+            placeholder="Enter your Item name"
+            required
+            className="form-input"
+          />
+        </label>
+
+        <label>
+          <span className=" font-semibold text-base text-gray-700">
+            อีเมลในการติดต่อ
+          </span>
+          <input
+            value={post.name}
+            onChange={(e) => setPost({ ...post, name: e.target.value })}
+            placeholder="Enter your Item name"
+            required
+            className="form-input"
+          />
+        </label>
+
+        <label>
+          <span className=" font-semibold text-base text-gray-700">
+            Image for your Item
+          </span>
+          <Dropzone post={post} setPost={setPost} />
+        </label>
+
+        <div className="flex-end mx-3 mb-5 gap-4">
+          <a href="/admin/" className="text-gray-500 text-md">
+            Cancel
+          </a>
+
+          <button
+            type="submit"
+            disabled={submitting}
+            className="text-white px-5 py-1.5 text-md rounded-full bg-primary-green"
+          >
+            sumbit
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default PublishPage;
--- app/research/page.tsx
+++ app/research/page.tsx
@@ -1,12 +1,16 @@
 import ResearchHeader from "@/components/ResearchHeader";
 import React from "react";
+import { AiOutlineFilePdf } from "react-icons/ai";
+import { AiOutlineFileExcel } from "react-icons/ai";
+import { AiOutlineFileWord } from "react-icons/ai";
+import { AiOutlineFile } from "react-icons/ai";
 
 const ResearchPage = () => {
   return (
     <div>
-      <li className="flex gap-6">
-        <div className="flex flex-col gap-3">
-          <ul className="flex flex-col gap-3 overflow-y-auto h-[80vh] pl-3 box-container">
+      <li className="flex gap-6 main-container justify-center">
+        <div className="flex flex-col gap-3  max-w-[65%]">
+          <ul className="flex flex-col gap-3 overflow-y-auto pl-3 box-container h-[60vh]">
             <ResearchHeader></ResearchHeader>
             <p className="paragraph">
               Lorem ipsum dolor sit amet consectetur, adipisicing elit.
@@ -22,12 +26,15 @@ const ResearchPage = () => {
               quidem adipisci. Eos, labore.
             </p>
           </ul>
-          <ul className="glassmorphism">
-            <h3>ทรัพยากร</h3>
-            <p>hello world</p>
+          <ul className="box-container">
+            <h3 className="mb-2">ทรัพยากร</h3>
+            <ul className="gap-2 flex flex-col">
+              <FileDownload />
+              <FileDownload />
+            </ul>
           </ul>
         </div>
-        <ul className="glassmorphism py-8 rounded-lg px-6 w-[50%] ">
+        <ul className="box-container flex flex-col gap-7 [&>li>h3]:mb-2">
           <li>
             <h3>องกรณ์</h3>
             <p>
@@ -35,10 +42,14 @@ const ResearchPage = () => {
               voluptates aut numquam rem esse, odit molestias
             </p>
           </li>
-          <ul className="flex flex-col gap-2">
+          <li className="flex flex-col">
             <h3>ชื่อผู้ค้นคว้า</h3>
             <ResearcherDetail />
-          </ul>
+          </li>
+          <li className="flex flex-col">
+            <h3>ติดต่อ</h3>
+            <p>sompon.sandee@gmail.com</p>
+          </li>
         </ul>
       </li>
     </div>
@@ -56,5 +67,16 @@ const ResearcherDetail = () => {
         aut numquam rem esse, odit molestias
       </p>
     </div>
+  );
+};
+
+const FileDownload = () => {
+  return (
+    <a>
+      <li className="border-black border-[1px] px-6 py-2 rounded-lg flex flex-row items-center">
+        <AiOutlineFilePdf className="w-[2.4rem] h-[2.4rem] fill-gray-700" />
+        <p className="text-center w-full">file_name.pdf</p>
+      </li>
+    </a>
   );
 };
