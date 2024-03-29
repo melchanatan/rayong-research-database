c5d29af48c3cfd2f5b9f620eb93bbe716761bd1b
melchanatan <chanatan@outlook.com>
2024-03-29 22:11:53 +0700

    fix: blob background move only on desktop

--- components/BlobBackground.tsx
+++ components/BlobBackground.tsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from "react";
 
 const BlobBackground: React.FC = () => {
+  const isMobile = window.innerWidth < 768;
   const blueGradient: string =
     "radial-gradient(circle, rgba(43,170,217,1) 0%, rgba(13,84,149,1) 68%)";
   const greenGradient: string =
@@ -83,6 +84,9 @@ const BlobBackground: React.FC = () => {
   }
 
   useEffect(() => {
+    if (isMobile) {
+      return;
+    }
     var interval = setInterval(moveBlob, 3000);
     return () => clearInterval(interval);
   }, []);
