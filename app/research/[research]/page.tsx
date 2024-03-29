2877504d813b35ea6d0637b850c55fe143a50a17
melchanatan <chanatan@outlook.com>
2024-03-29 21:59:21 +0700

    fix: import typo

--- app/research/[research]/page.tsx
+++ app/research/[research]/page.tsx
@@ -3,7 +3,7 @@ import DynamicFileIcon from "@/components/DynamicFileIcon";
 import ParagraphSkeleton from "@/components/ParagraphSkeleton";
 import ResearchHeader from "@/components/ResearchHeader";
 import TabNav from "@/components/TabNav";
-import formatDateToThai from "@/utils/formatDateToThai";
+import formatDateToThai from "@/utils/FormatDateToThai";
 import React, { useState } from "react";
 import { FaLessThanEqual } from "react-icons/fa6";
 
